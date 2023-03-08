b71fc07585ff46f005007ea5c761ec5ea71ea212
Marcos Velazquez <marcvbusiness@gmail.com>
2023-03-07 23:42:41 -0600

    Refactor core settings

---
+++ lua/mvsolves/core/colorscheme.lua
@@ -0,0 +1,8 @@
+-- Colorscheme
+-- vim.cmd('colorscheme afterglow')
+vim.cmd('colorscheme hybrid')
+
+-- Line number
+vim.opt.number = true -- enable line number
+vim.cmd('highlight LineNr ctermfg=black ctermbg=grey')
+vim.opt.numberwidth = 4 -- tighten the line number width
--- lua/mvsolves/core/options.lua
+++ lua/mvsolves/core/options.lua
@@ -1,23 +1,12 @@
--- Disable netrw at the very start of your init.lua (strongly advised)
-vim.g.loaded_netrw = 1
-vim.g.loaded_netrwPlugin = 1
-
 -- Nvim setup
-vim.cmd('syntax on')
--- vim.cmd('colorscheme materialbox')
-vim.cmd('colorscheme jellybeans')
-
-vim.opt.number = true -- enable line number
-vim.cmd('highlight LineNr ctermfg=black ctermbg=grey')
-vim.opt.numberwidth = 4 -- tighten the line number width
-
 vim.opt.showcmd = false -- hide the commands
 vim.opt.showmode = false -- hide the --INSERT--
 vim.opt.cmdheight = 1
 
-vim.opt.timeoutlen = 1000 -- for mapping delays,
+vim.opt.timeoutlen = 300 -- for mapping delays,
 vim.opt.ttimeoutlen = 0 -- used for key code delays
 
+-- The autocmds automatically save and update buffer if changes made externally
 vim.cmd("autocmd BufWinLeave * silent! mkview!")
 vim.cmd("autocmd BufWinEnter * silent! loadview")
 
@@ -33,6 +22,3 @@ vim.cmd('au BufNewFile,BufRead *.ejs set filetype=html')
 
 -- Contains MV custom snippets
 vim.cmd('source $HOME/.snippets/snippets.vim')
-
--- Vimwiki plugin setup
-vim.g.vimwiki_list = { { path = '~/Documents/vimwiki', syntax = 'markdown', ext = '.md' } }
