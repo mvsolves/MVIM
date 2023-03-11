d07e79ac45cdc86ba0a92665535dc36e63aa5ed4
Marcos Velazquez <marcvbusiness@gmail.com>
2023-03-10 23:07:11 -0600

    Add function to change colorscheme when extension 'vimwiki'

--- lua/mvsolves/core/colorscheme.lua
+++ lua/mvsolves/core/colorscheme.lua
@@ -1,8 +1,24 @@
+local cmd = vim.cmd
+local opt = vim.opt
+
 -- Colorscheme
 -- vim.cmd('colorscheme afterglow')
-vim.cmd('colorscheme hybrid')
+cmd('colorscheme hybrid')
 
 -- Line number
-vim.opt.number = true -- enable line number
-vim.cmd('highlight LineNr ctermfg=black ctermbg=grey')
-vim.opt.numberwidth = 4 -- tighten the line number width
+opt.number = true -- enable line number
+cmd('highlight LineNr ctermfg=black ctermbg=grey')
+opt.numberwidth = 4 -- tighten the line number width
+
+function updateColorScheme()
+  if vim.bo.filetype == 'vimwiki' then
+    cmd('colorscheme purify')
+  else
+    cmd('colorscheme hybrid')
+    opt.number = true -- enable line number
+    cmd('highlight LineNr ctermfg=black ctermbg=grey')
+    opt.numberwidth = 4 -- tighten the line number width
+  end
+end
+
+cmd('autocmd BufEnter * lua updateColorScheme()')
